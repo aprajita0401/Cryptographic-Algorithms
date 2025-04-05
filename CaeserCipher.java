a0ca5ffd78130a8b40ec3e8da67558fdd578e382
Aprajita Nandkeuliar <145915093+aprajita0401@users.noreply.github.com>
2025-04-06 01:26:21 +0530

    Add files via upload

---
+++ CaeserCipher.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class CaeserCipher {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string to encrypt: ");
+        String str = sc.nextLine().toLowerCase(); // Convert input to lowercase
+
+        caesarCipher(str);
+    }
+    //Caeser cipher encrypts only alphabets from A-Z or a-z
+    public static StringBuilder caesarCipher(String input) {
+        StringBuilder cipheredText = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            //add any white space to the cipheredText (whitespaces do not get ciphered in Caeser cipher)
+            if (ch == ' ') {
+                cipheredText.append(' ');
+            }
+            else {
+                int index = ch - 'a'; //to find ASCII value of ch
+                int shiftedIndex = (index + 3) % 26; //caesar cipher formula
+                char encryptedChar = (char) ('a' + shiftedIndex); //converting ASCII value to char
+                cipheredText.append(encryptedChar);
+            }
+        }
+        return cipheredText;
+    }
+}
